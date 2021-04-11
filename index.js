125fe6b6214857487b17d3348ca531e429f2b8c3
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-11 12:29:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 document.querySelector("main#main").remove();
 let newHeader = document.createElement("h1");
 /*header.innerHTML = 'Abdallah is the champion';
-document.body.appendChild(header);
+document.body.appendChild(header);*/
