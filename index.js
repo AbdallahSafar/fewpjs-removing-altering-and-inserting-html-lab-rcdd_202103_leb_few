dea51ee9e1f0c0ba77094f12266cf31021cbe574
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-11 12:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 document.querySelector("main#main").remove();
-let header = document.createElement("h1#victory"));
+let header = document.createElement("h1"));
 header.innerHTML = 'newHeader';
 document.body.appendChild(header);
